8206501315c25e1b05707d6e79c8b2ffd89cc947
byebyers <byers.jacob@gmail.com>
2023-02-06 17:14:46 -0700

    init

---
+++ plugins/create-block-theme/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '31b0f8247726f0b2a608');
